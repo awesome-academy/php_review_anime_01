c49c257be344ee69171e64adfdd0225b57f40f65
killthemall223 <lehoangson9674@gmail.com>
2018-10-17 15:46:45 +0700

    create_migration

---
+++ database/migrations/2018_10_17_030512_create_episodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('anime_list_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->string('video');
+            $table->float('rating');
+            $table->integer('view_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
